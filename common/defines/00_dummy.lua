88e4dc4595c57cca3732c84782b80f12db5306ba
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2022-03-09 16:05:12 +0200

    Bug fixes

--- common/defines/00_dummy.lua
+++ common/defines/00_dummy.lua
@@ -2,6 +2,11 @@
 --
 -- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
 
-    NDefines.NMilitary.DAYS_PER_PHASE = 2
-    NDefines.NMilitary.DAYS_PER_SIEGE_PHASE = 20
-    NDefines.NCountry.REGULAR_COLONY_GROWTH = 20
+-- Custom defines to make combat faster:
+
+NDefines.NMilitary.DAYS_PER_PHASE = 2
+NDefines.NMilitary.DAYS_PER_SIEGE_PHASE = 20
+
+-- Custom defines to slow down colonization:
+
+NDefines.NCountry.REGULAR_COLONY_GROWTH = 20
