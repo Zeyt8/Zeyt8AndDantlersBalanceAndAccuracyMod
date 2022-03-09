fdbffd228b634830cbaad7cf8950dc691fdfc8f8
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2022-03-09 10:43:59 +0200

    Lowered colonisation. Sped up wars. Changes to start map.. Harder to get military access.
    Lowered colonization buffs from technology and Portugal age bonus.
    Reduced combat phases and siege phases duration.
    Made hungarian vassals start as vassals.
    Made military access require 50 relations and not being a vassal of a rival.

---
+++ common/defines/00_dummy.lua
@@ -0,0 +1,7 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+    NDefines.NMilitary.DAYS_PER_PHASE = 2
+    NDefines.NMilitary.DAYS_PER_SIEGE_PHASE = 20
+    NDefines.NCountry.REGULAR_COLONY_GROWTH = 20
