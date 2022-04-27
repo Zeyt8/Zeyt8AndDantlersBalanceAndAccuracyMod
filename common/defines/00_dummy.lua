311b2c01645297814b4ee47a83256b2ec39c4ec7
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2022-04-27 14:55:54 +0300

    Changed modifiers. Added albanian culture group
    Added file for adjacencies without navigable rivers.
    Added albanian culture group.
    Increased state maintanance modifier for unaccepted culture from 0.25 to 0.35.
    Lowered cost for artillery and naval barrage from 50 to 40.
    Reduced tropical, arctic and arid colonial growth from -10 to -15.
    Added -trade goods and trade power from blockaded provinces.
    Increased unrest from unaccepted culture from 2 to 3
    Added penalty to tax and increased unrest from devastation.
    Added reduce missionary strength from tolerance.
    Added reduced morale and liberty desire from war exhaustion.
    Added increased manpower from negative piety.
    Added lower discipline and army professionalism decay from corruption.
    Added lower loyalty from estates when rooting out corruption.
    Added tolerance to republican tradition.
    Changed modifiers of army professionalism.

--- common/defines/00_dummy.lua
+++ common/defines/00_dummy.lua
@@ -2,13 +2,7 @@
 --
 -- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
 
--- Custom defines to make combat faster:
-
-NDefines.NMilitary.DAYS_PER_PHASE = 2 -- from 3
-NDefines.NMilitary.DAYS_PER_SIEGE_PHASE = 20 -- from 30
-NDefines.NMilitary.FRONT_LINE_MODIFIER = 2.0 -- from 1.0
-NDefines.NMilitary.BACK_LINE_MODIFIER = 1.0 -- from 0.5
-NDefines.NMilitary.CANNOT_RETREAT_DAYS = 6 -- from 12
+-- NCountry --
 
 -- Custom defines to slow down colonization:
 
@@ -16,6 +10,7 @@ NDefines.NCountry.REGULAR_COLONY_GROWTH = 20 -- from 25
 
 -- Custom defines to increase state maintenance
 NDefines.NCountry.STATE_MAINTENANCE_DISTANCE_FACTOR = 0.002 -- from 0.001
+NDefines.NCountry.STATE_MAINTENANCE_CULTURE_FACTOR = 0.35 -- from 0.25
 NDefines.NCountry.STATE_MAINTENANCE_DEV_FACTOR = 0.007 -- not changed
 NDefines.NCountry.STATE_MAINTENANCE_CONTINENT_FACTOR = 0.25 -- not changed
 
@@ -23,6 +18,17 @@ NDefines.NCountry.STATE_MAINTENANCE_CONTINENT_FACTOR = 0.25 -- not changed
 
 NDefines.NCountry.NEW_HEIR_PRESTIGE_HIT = -30 -- from -20
 NDefines.NCountry.NEW_HEIR_LEGITIMACY_HIT = -30 -- from -20
+
+-- Custom defines to national idea aquisition
+NDefines.NCountry.FREE_IDEA_GROUP_COST = 4 -- from 3
+
+-- Reduce combat actions cost to make them usable --
+NDefines.NCountry.PS_ASSAULT = 5
+NDefines.NCountry.PS_ARTILLERY_BARRAGE = 40 -- from 50
+NDefines.NCountry.PS_NAVAL_BARRAGE = 40 -- from 50
+
+
+-- NEconomy --
 
 -- Custom defines to increase army and navy maintenance
 
@@ -33,24 +39,30 @@ NDefines.NEconomy.NAVAL_TECH_MAINTENANCE_IMPACT = 0.03 -- from 0.02
 NDefines.NEconomy.GOLD_INFLATION = 0.75 -- from 0.5
 NDefines.NEconomy.TREASURE_FLEET_INFLATION = 0.75 -- from 0.5
 
+-- Reduce mercenary late game cost --
+NDefines.NEconomy.LAND_TECH_MERC_MAINTENANCE_IMPACT = 0.08 -- from 0.08
+NDefines.NEconomy.MERCENARY_COMPANY_MAX_REGIMENTS = 40 -- from 60
+
+
+-- NMilitary -- 
+
 -- Custom defines to nerf slacken recruitment
 NDefines.NMilitary.SLACKEN_AP_DROP = 0.075 -- from 0.05
+
+-- Custom defines to buff cavalry
+NDefines.NMilitary.CAVALRY_COST = 23 -- from 25
+
+-- Custom defines to make combat faster:
+NDefines.NMilitary.DAYS_PER_PHASE = 2 -- from 3
+NDefines.NMilitary.DAYS_PER_SIEGE_PHASE = 20 -- from 30
+NDefines.NMilitary.FRONT_LINE_MODIFIER = 2.0 -- from 1.0
+NDefines.NMilitary.BACK_LINE_MODIFIER = 1.0 -- from 0.5
+NDefines.NMilitary.CANNOT_RETREAT_DAYS = 6 -- from 12
+
+-- NAI --
 
 -- Custom defines for AI
 NDefines.NAI.PEACE_TIME_EARLY_FACTOR = 0.6 -- from 0.75
 NDefines.NAI.PEACE_TIME_MAX_MONTHS = 540 -- from 600
 
 NDefines.NReligion.PICK_GOLDEN_BULL_COST = 500 -- from 400
-
--- Custom defines for army drill
-NDefines.NMilitary.ARMY_DRILL_YEARLY_DECAY = -0.5 -- from -1
-
--- Custom defines to buff cavalry
-NDefines.NMilitary.CAVALRY_COST = 23 -- from 25
-
--- Custom defines to national idea aquisition
-NDefines.NCountry.FREE_IDEA_GROUP_COST = 4 -- from 3
-
--- Reduce mercenary late game cost --
-NDefines.NEconomy.LAND_TECH_MERC_MAINTENANCE_IMPACT = 0.08 -- from 0.08
-NDefines.NEconomy.MERCENARY_COMPANY_MAX_REGIMENTS = 40 -- from 60
