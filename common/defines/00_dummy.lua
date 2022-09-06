2474667d9650e06fbeafec2a383826e1145a7877
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2022-09-06 17:19:59 +0300

    Prep for 1.34

--- common/defines/00_dummy.lua
+++ common/defines/00_dummy.lua
@@ -34,17 +34,11 @@ NDefines.NCountry.MONTHS_TO_CHANGE_CULTURE = 15
 
 -- NEconomy --
 
--- Custom defines to increase army and navy maintenance
-
---NDefines.NEconomy.LAND_TECH_MAINTENANCE_IMPACT = 0.03 -- from 0.02
---NDefines.NEconomy.NAVAL_TECH_MAINTENANCE_IMPACT = 0.03 -- from 0.02
-
 -- Custom defines to increase inflation from gold and treasure fleets
 NDefines.NEconomy.GOLD_INFLATION = 0.75 -- from 0.5
 NDefines.NEconomy.TREASURE_FLEET_INFLATION = 0.75 -- from 0.5
 
 -- Reduce mercenary late game cost --
---NDefines.NEconomy.LAND_TECH_MERC_MAINTENANCE_IMPACT = 0.08 -- from 0.08
 NDefines.NEconomy.MERCENARY_COMPANY_MAX_REGIMENTS = 40 -- from 60
 
 
